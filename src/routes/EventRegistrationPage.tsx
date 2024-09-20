d9a451c4f4d85e56a3def636b590997fa00e9f7b
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-09-21 00:51:38 +0300

    add participant adding logic

--- src/routes/EventRegistrationPage.tsx
+++ src/routes/EventRegistrationPage.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import { ParticipantInput } from "../helpers/types";
+import { addParticipantToEvent } from "../helpers/api";
 
 const EventRegistrationPage = () => {
   const { id } = useParams<{ id: string }>();
   const { register, handleSubmit } = useForm<ParticipantInput>();
 
-  const onSubmit = (data: ParticipantInput) => {
-    console.log(data);
+  const onSubmit = async (data: ParticipantInput) => {
+    if (id) {
+      console.log(data);
+
+      await addParticipantToEvent(id, data);
+    } else {
+      console.error("Event ID is undefined");
+    }
   };
 
   return (
@@ -40,17 +47,17 @@ const EventRegistrationPage = () => {
             />
           </label>
           <label>
-            Email:
+            Friend:
             <input
               type="radio"
-              value="Friends"
+              value="Friend"
               {...register("heard_about_us", {
                 required: "Please select how you heard about us",
               })}
             />
           </label>
           <label>
-            Email:
+            Social Media:
             <input
               type="radio"
               value="Social Media"
@@ -60,7 +67,7 @@ const EventRegistrationPage = () => {
             />
           </label>
           <label>
-            Email:
+            Advertisement:
             <input
               type="radio"
               value="Advertisement"
@@ -77,85 +84,3 @@ const EventRegistrationPage = () => {
 };
 
 export default EventRegistrationPage;
-
-// import React from "react";
-// import { useForm } from "react-hook-form";
-// import { useParams } from "react-router-dom";
-// import { ParticipantInput } from "../helpers/types";
-
-// const EventRegistrationPage = () => {
-//   const { id } = useParams<{ id: string }>();
-//   const { register, handleSubmit } = useForm<ParticipantInput>();
-
-//   const onSubmit = (data: ParticipantInput) => {
-//     console.log(data);
-//   };
-
-//   return (
-//     <section className="eventRegistration">
-//       <div className="eventRegistration__wrapper">
-//         <h1 className="eventRegistration__title">Event Registration</h1>
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//           <label>
-//             Name:
-//             <input
-//               type="text"
-//               {...register("name", { required: "Name is required" })}
-//             />
-//           </label>
-//           <label>
-//             Email:
-//             <input
-//               type="email"
-//               {...register("email", { required: "Email is required" })}
-//             />
-//           </label>
-//           <label>
-//             Date of Birth:
-//             <input
-//               type="date"
-//               {...register("date_of_birth", {
-//                 required: "Date of birth is required",
-//               })}
-//             />
-//           </label>
-//           <div>
-//             <label>
-//               <input
-//                 type="radio"
-//                 value="Friends"
-//                 {...register("heard_about_us", {
-//                   required: "Please select how you heard about us",
-//                 })}
-//               />
-//               Friends
-//             </label>
-//             <label>
-//               <input
-//                 type="radio"
-//                 value="Social Media"
-//                 {...register("heard_about_us", {
-//                   required: "Please select how you heard about us",
-//                 })}
-//               />
-//               Social Media
-//             </label>
-//             <label>
-//               <input
-//                 type="radio"
-//                 value="Advertisement"
-//                 {...register("heard_about_us", {
-//                   required: "Please select how you heard about us",
-//                 })}
-//               />
-//               Advertisement
-//             </label>
-//           </div>
-//           <button type="submit">Submit</button>
-//         </form>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default EventRegistrationPage;
