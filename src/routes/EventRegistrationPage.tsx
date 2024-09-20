cddfdb47674370936047d713680430e57c444a23
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-09-20 23:54:43 +0300

    add reacthookform basic logic

--- src/routes/EventRegistrationPage.tsx
+++ src/routes/EventRegistrationPage.tsx
@@ -1,42 +1,161 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
+import { ParticipantInput } from "../helpers/types";
 
 const EventRegistrationPage = () => {
   const { id } = useParams<{ id: string }>();
+  const { register, handleSubmit } = useForm<ParticipantInput>();
 
-  const handleSubmit = (e: any, values: any) => {
-    e.preventDefault;
-    console.log(values);
+  const onSubmit = (data: ParticipantInput) => {
+    console.log(data);
   };
 
   return (
     <section className="eventRegistration">
       <div className="eventRegistration__wrapper">
         <h1 className="eventRegistration__title">Event Registration</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>
-            <input type="text" name="name" />
+            Name:
+            <input
+              type="text"
+              {...register("name", { required: "Name is requires" })}
+            />
           </label>
           <label>
-            <input type="mail" name="email" />
+            Email:
+            <input
+              type="email"
+              {...register("email", { required: "Email is required" })}
+            />
           </label>
           <label>
-            <input type="date" name="date_of_birth" />
+            Date of birth:
+            <input
+              type="date"
+              {...register("date_of_birth", {
+                required: "Date of birth is required",
+              })}
+            />
           </label>
           <label>
-            <input type="radio" name="heard_about_us" value="Friends" />
+            Email:
+            <input
+              type="radio"
+              value="Friends"
+              {...register("heard_about_us", {
+                required: "Please select how you heard about us",
+              })}
+            />
           </label>
           <label>
-            <input type="radio" name="heard_about_us" value="Social Media" />
+            Email:
+            <input
+              type="radio"
+              value="Social Media"
+              {...register("heard_about_us", {
+                required: "Please select how you heard about us",
+              })}
+            />
           </label>
           <label>
-            <input type="radio" name="heard_about_us" value="Advertisement" />
+            Email:
+            <input
+              type="radio"
+              value="Advertisement"
+              {...register("heard_about_us", {
+                required: "Please select how you heard about us",
+              })}
+            />
           </label>
+          <button type="submit">submit</button>
         </form>
       </div>
-      <button type="submit">submit</button>
     </section>
   );
 };
 
 export default EventRegistrationPage;
+
+// import React from "react";
+// import { useForm } from "react-hook-form";
+// import { useParams } from "react-router-dom";
+// import { ParticipantInput } from "../helpers/types";
+
+// const EventRegistrationPage = () => {
+//   const { id } = useParams<{ id: string }>();
+//   const { register, handleSubmit } = useForm<ParticipantInput>();
+
+//   const onSubmit = (data: ParticipantInput) => {
+//     console.log(data);
+//   };
+
+//   return (
+//     <section className="eventRegistration">
+//       <div className="eventRegistration__wrapper">
+//         <h1 className="eventRegistration__title">Event Registration</h1>
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <label>
+//             Name:
+//             <input
+//               type="text"
+//               {...register("name", { required: "Name is required" })}
+//             />
+//           </label>
+//           <label>
+//             Email:
+//             <input
+//               type="email"
+//               {...register("email", { required: "Email is required" })}
+//             />
+//           </label>
+//           <label>
+//             Date of Birth:
+//             <input
+//               type="date"
+//               {...register("date_of_birth", {
+//                 required: "Date of birth is required",
+//               })}
+//             />
+//           </label>
+//           <div>
+//             <label>
+//               <input
+//                 type="radio"
+//                 value="Friends"
+//                 {...register("heard_about_us", {
+//                   required: "Please select how you heard about us",
+//                 })}
+//               />
+//               Friends
+//             </label>
+//             <label>
+//               <input
+//                 type="radio"
+//                 value="Social Media"
+//                 {...register("heard_about_us", {
+//                   required: "Please select how you heard about us",
+//                 })}
+//               />
+//               Social Media
+//             </label>
+//             <label>
+//               <input
+//                 type="radio"
+//                 value="Advertisement"
+//                 {...register("heard_about_us", {
+//                   required: "Please select how you heard about us",
+//                 })}
+//               />
+//               Advertisement
+//             </label>
+//           </div>
+//           <button type="submit">Submit</button>
+//         </form>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default EventRegistrationPage;
